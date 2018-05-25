dfe7e6ad9dc83baeb03c1482c8eb8546f5069148
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-05-25 22:28:26 +0200

    Add enum to represent different game statuses

---
+++ src/main/java/com/durance/kata/game/Status.java
@@ -0,0 +1,11 @@
+package com.durance.kata.game;
+
+/**
+ * Enum to represent different game statuses
+ * @author Solange U. Gasengayire
+ */
+public enum  Status {
+    DRAW,
+    INCOMPLETE,
+    WIN
+}
